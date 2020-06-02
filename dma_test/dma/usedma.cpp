0eed144a3676c9c59ea8987d7019e3da1e9613f6
achrypadusi <p.stelmachowski@gmail.com>
2020-06-02 10:07:21 +0200

    exc 13.3

--- dma_test/dma/usedma.cpp
+++ dma_test/dma/usedma.cpp
@@ -6,29 +6,62 @@ int main()
 {
 	using std::cout;
 	using std::endl;
-	baseDMA shirt("Portabelly", 8);
+	/*baseDMA shirt("Portabelly", 8);
 	lacksDMA balloon("red", "Blimpo", 4);
 	hasDMA map("Mercator", "Buffalo Keys", 5);
 	cout << "Displaying baseDMA object:\n";
-	cout << shirt << endl;
+	shirt.view();
+	//cout << shirt << endl;
 	cout << "Displaying lacksDMA object:\n";
-	cout << balloon << endl;
+	balloon.view();
+	//cout << balloon << endl;
 	cout << "Displaying hasDMA object:\n";
-	cout << map << endl;
+	map.view();
+	//cout << map << endl;
 	lacksDMA balloon2(balloon);
 	cout << "Result of lacksDMA copy:\n";
-	cout << balloon2 << endl;
+	balloon2.view();
+	//cout << balloon2 << endl;
 	hasDMA map2;
 	map2 = map;
 	cout << "Result of hasDMA assignment:\n";
-	cout << map2 << endl;
+	map2.view();*/
+	//cout << map2 << endl;
 
-	baseDMA map3;
-	map3 = map2;
-	cout << "Result of baseDMA assignment:\n";
-	cout << map3 << endl;
-
-
-
+	cout << "Select class: " << endl;
+	cout << "baseDMA - 1" << endl << "lackDMA - 2" << endl << "hasDMA - 3" << endl;
+	char option;
+	while (option = std::cin.get(), option != 'q')
+	{
+		switch (option)
+		{
+		case '1':
+		{
+			cout << "baseDMA" << endl;
+			baseDMA* shirt = new baseDMA("Portabelly", 8);
+			shirt->view();
+			delete shirt;
+			break;
+		}
+		case '2':
+		{
+			cout << "lackDMA" << endl;
+			lacksDMA* balloon = new lacksDMA("red", "Blimpo", 4);
+			balloon->view();
+			delete balloon;
+			break;
+		}
+		case '3':
+		{
+			cout << "hasDMA" << endl;
+			hasDMA* map = new hasDMA("Mercator", "Buffalo Keys", 5);
+			map->view();
+			delete map;
+			break;
+		}
+		default:
+			cout << "wrong option" << endl;
+		}
+	}
 	return 0;
 }
