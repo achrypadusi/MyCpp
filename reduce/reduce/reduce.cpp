412991bd7efd9502e2dbe0527c972e8821d69149
achrypadusi <p.stelmachowski@gmail.com>
2020-07-11 22:18:21 +0200

    template

--- reduce/reduce/reduce.cpp
+++ reduce/reduce/reduce.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <algorithm>
 
-int reduce(long ar[], int n);
+template <typename T>
+int reduce(T ar[], int n);
 
 int main()
 {
 	using namespace std;
 	
-	long ar[] = { 3, 1, 2 ,3, 7, 1 };
-	int sizeAr = sizeof(ar) / sizeof(long);
+	double ar[] = { 3, 1, 2 ,3, 7.2, 1 };
+	int sizeAr = sizeof(ar) / sizeof(double);
 	cout << "size of array: " << sizeAr << endl;
 
 	cout << "size of reduced: " << reduce(ar, sizeAr) << endl;
 }
 
-int reduce(long ar[], int n) {
+template <typename T>
+int reduce(T ar[], int n) {
 	std::sort(ar, ar + n);
-	long* lastOne = std::unique(ar, ar + n);
+	T* lastOne = std::unique(ar, ar + n);
 	n = lastOne - ar;
 	for (int i = 0; i < n; i++)
-		std::cout << i << " ";
+		std::cout << ar[i] << " ";
 	std::cout << std::endl;
 	return n;
 }
